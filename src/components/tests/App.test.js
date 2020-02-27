3f7e9398ab9ee04cfd966ad8af41e3427f16a293
samuelmorenov <UO266321@uniovi.es>
2020-02-28 00:48:21 +0100

    Añadido import que daba error al hacer test

--- src/components/tests/App.test.js
+++ src/components/tests/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
 import App from '../App';
 
 test('renders proyecto en github link', () => {

fe1201d76e4ab9b607f6bf3be897bd7bdf40f47a
Alvatros96 <UO245852@uniovi.es>
2020-03-09 00:00:39 +0100

    Creacion de imagenes viaDe y insercion en codigo

--- src/components/NavBar/nav-bar.component.js
+++ src/components/NavBar/nav-bar.component.js
@@ -58,7 +58,7 @@ const NavBar = (props: Props) => {
       <section className="header-wrap">
         <div className="logo-block">
           <Link to="/welcome">
-            <img src="/img/inrupt.svg" alt="inrupt" />
+            <img src="/img/viaDe.svg" alt="inrupt" />
           </Link>
         </div>
 

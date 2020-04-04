dbf10bbd951f559ae60c88a3c14458f0609f5c0b
Alvatros96 <UO245852@uniovi.es>
2020-04-04 23:39:38 +0200

    Cambios en ShowFriends

---
+++ src/components/share/ShowFriends.js
@@ -0,0 +1,18 @@
+import React from "react";
+const fc = require("solid-file-client");
+
+
+class ShowFriends extends React.Component {
+
+
+
+  render() {
+    return (
+      <div>
+          <h2>Lista de amigos</h2>
+      </div>
+    );
+  }
+}
+
+export default ShowFriends;

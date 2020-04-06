84632c09c513f3420271a2db8f8e1aa46deb4901
Adrián Fernández Alonso <adrianfutbolin@gmail.com>
2020-04-06 17:44:16 +0200

    Mas experimetos con las pruebas

--- src/components/login/LoginButton.js
+++ src/components/login/LoginButton.js
@@ -7,7 +7,7 @@ const popUri = "https://solid.community/common/popup.html";
 export default class LoginButton extends React.Component {
     render() {
         return (
-            <div id = "botonLogin">
+            <div>
                 <AuthButton className="btn" popup={popUri} login="Identificate" logout="Desconectar" />
             </div>
         );
--- src/components/tests/App.test.js
+++ src/components/tests/App.test.js
@@ -3,15 +3,41 @@ import { render } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import Main from "../Main";
 import { Given, Then, When } from "jest-cucumber"; 
+import { create, act } from "react-test-renderer";
+import { LoginButton, LoggedOut } from "@solid/react";
+import ReactDom from "react-dom";
 
-test("loggearse", () => {
+test("prueba1", () => {
   //render(<Main/>);
   //const contain = document.getElementsByClassName("container");
   //const container = contain[0];
   //const containerHijos = container.hasChildNodes();
   //expect(containerHijos).toBeTruthy();
 
-  Given("Estoy en la pagina principal sin estar logeado", function(){
-    this.setRoute("/");
+  //Given("Estoy en la pagina principal sin estar logeado", function(){
+  //  this.setRoute("/");
+  //});
+
+  const main = create(<Main/>);
+  expect(main).toMatchSnapshot();
+ 
+  
+  const loginbutton = create(<LoginButton/>);
+  expect(loginbutton).toMatchSnapshot();
+
+  const loggedOut = create(<LoggedOut></LoggedOut>)
+  expect(loggedOut).toMatchSnapshot();
+
+});
+
+test("prueba2", () => {
+  let container = document.createElement('div');
+  document.body.append(container);
+
+  act(()=>{
+    ReactDom.render(<Main></Main>, container);
   });
+
+  const boton = container.querySelector('button');
+  expect(boton.textContent).toBe('Identificate');
 });

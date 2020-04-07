138273b952a6546c99c25a7fc08f260672e1d217
samuelmorenov <UO266321@uniovi.es>
2020-04-07 17:24:48 +0200

    Ahora leemos el archivo en el input de compartir archivo

--- src/components/share/Share.js
+++ src/components/share/Share.js
@@ -3,39 +3,105 @@ import React from "react";
 import { useWebId } from "@solid/react";
 import InputField from "../basics/ImputField";
 import Button from "../basics/BasicButton";
+import FileClient from "solid-file-client";
+import auth from "solid-auth-client";
 
 export const Hook = () => {
     let webid = String(String(useWebId()).replace("/profile/card#me", "/public/rutas/"));
+
+
     class Share extends React.Component {
         constructor(props) {
             super(props);
             this.state = {
-                archivo: webid,
-                amigo: ""
+                archivo: "https://samuelmorenov.solid.community/public/rutas/prueba5.geojson",
+                amigo: "https://samuelmorenov.solid.community/profile/card#me",
+                //archivo: webid,
+                //amigo: ""
             };
         }
 
         async enviar() {
-            // if (!this.state.archivo) {
-            //   return;
-            // }
-            // if (!this.state.amigo) {
-            //   return;
-            // }
-            alert("Enviando " + this.state.archivo + " a " + this.state.amigo)
+            if (!this.state.archivo) {
+                return;
+            }
+            if (!this.state.amigo) {
+                return;
+            }
+
+            //Leer archivo
+
+            const auth = require("solid-auth-client");
+            const FC = require("solid-file-client");
+            const fc = new FC(auth);
+
+            let archivoLeido;
+            await fc.readFile(this.state.archivo)
+                .then(content => archivoLeido = content)
+                .catch(err => (archivoLeido = null));
+
+            console.log(archivoLeido)
+
+            //Guardar archivo
+            let webidfriend = String(String(this.state.amigo).replace("/profile/card#me", "/public/rutasCompartidas/"));
+
+
+
+
+            
+
+            alert("Enviado " + this.state.archivo + " a " + this.state.amigo);
         }
+
+        // handleFiles = (files) => {
+        //     let webid = String(String(useWebId()).replace("/profile/card#me", "/public/rutas/"));
+
+        //     var reader = new FileReader();
+
+        //     reader.onload = function () {
+        //         let fc = new FileClient(auth);
+        //         let fileName = files[0].name;
+        //         let url = webid + fileName;
+        //         fc.createFile(url, reader.result, "text/turtle");
+        //         alert("Archivo subido a rutas/" + fileName);
+        //     };
+        //     reader.readAsText(files[0]);
+        // };
+
+        // getFiles() {
+        //     let session = await auth.currentSession();
+
+        //     const profileDocument = await fetchDocument(session.webId);
+        //     const profile = profileDocument.getSubject(session.webId);
+
+        //     const storage = profile.getRef(space.storage);
+
+        //     let folder;
+        //     await fc.readFolder(storage + "public/rutas/")
+        //       .then(content => { folder = content; })
+        //       .catch(err => (folder = null));
+
+        //     var filesObtained = [];
+        //     if (folder) {
+        //       for (var i = 0; i < folder.files.length; i++) {
+        //         let ruta = folder.files[i];
+        //         if (ruta != null)
+        //           filesObtained = [...filesObtained, ruta];
+        //       }
+        //     }
+        //     return filesObtained;
+        //   }
 
         setInputValue(property, val) {
             val = val.trim();
             this.setState({
                 [property]: val
             });
-            console.log(val)
         }
 
         handleChange(e) {
             this.setState({
-                amigo:e.target.value
+                amigo: e.target.value
             });
         }
 

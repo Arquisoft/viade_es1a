a004daa903618e7ee31b85d92e1342b34f3e73f8
samuelmorenov <UO266321@uniovi.es>
2020-04-08 19:28:26 +0200

    Ahora el hook notifications es un componente

--- src/components/notifications/Notification.js
+++ src/components/notifications/Notification.js
@@ -1,9 +1,51 @@
 import React from "react";
 import campanita from "../../static/images/campanita.png";
+import { space } from "rdf-namespaces";
+import { fetchDocument } from "tripledoc";
+import properties from "../commons/Properties";
 
-function Notification() {
-    return (
-    <img src={campanita} className="Campanita-ico" alt="ico" />
-    );
-};
+
+
+
+async function getNNotifications() {
+    const auth = require("solid-auth-client");
+    const FC = require("solid-file-client");
+    const fc = new FC(auth);
+
+    let session = await auth.currentSession();
+
+    const profileDocument = await fetchDocument(session.webId);
+    const profile = profileDocument.getSubject(session.webId);
+
+    const storage = profile.getRef(space.storage);
+
+    let folder;
+    await fc.readFolder(storage + properties.inbox)
+        .then(content => { folder = content; })
+        .catch(err => (folder = null));
+
+
+    return folder.files.length;
+}
+
+class Notification extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            nNotifications: 0,
+            inboxUrl: "https://ejemplo.solid.community/inbox/"
+        };
+      }
+
+    render() {
+        return (
+            <p>
+                <img src={campanita} className="Campanita-ico" alt="ico" />
+                <a href={this.state.inboxUrl}>Notificaciones recibidas</a>: {this.state.nNotifications}
+            </p>
+        );
+    }
+}
+
 export default Notification;

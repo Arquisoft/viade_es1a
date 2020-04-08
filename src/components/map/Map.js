31116dc00a09700e24771ba0512fcbdbea890f53
samuelmorenov <UO266321@uniovi.es>
2020-04-09 01:16:06 +0200

    Separados url de layers

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -4,40 +4,31 @@ import "leaflet/dist/leaflet.css";
 import styled from "styled-components";
 import Button from "../basics/BasicButton";
 import MapList from "../solidPod/MapList";
+import properties from "../commons/Properties";
 
 const Wrapper = styled.div`
     width: 900px;
     height: 600px;
 `;
 
-const urllayerMontania = "https://{s}.tile.thunderforest.com/spinal-map/{z}/{x}/{y}.png";
-const urllayerSatelite = "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}";
-
-const layer1 = L.tileLayer(urllayerMontania, {
-    detectRetina: true,
-    maxZoom: 20,
-    maxNativeZoom: 17
-});
-
-const layer2 = L.tileLayer(urllayerSatelite, {
-    detectRetina: true,
-    maxZoom: 20,
-    maxNativeZoom: 17
-});
-
-
 class Map extends React.Component {
 
     constructor(props) {
         super(props);
-
-        this.state = {
-            nlayer: 1,
-            layer: layer1
-        };
+        this.actual = 0;
+        this.layer = null;
     }
 
-    handleFiles(fileJson)  {
+    componentDidMount() {
+        this.map = L.map("map", {
+            center: [43.3602900, -5.8447600],
+            zoom: 10,
+            zoomControl: false
+        });
+        this.setLayer();
+    }
+
+    handleFiles(fileJson) {
         let center = [];
         center[0] = fileJson.features[0].geometry.coordinates[0][1];
         center[1] = fileJson.features[0].geometry.coordinates[1][0];
@@ -46,32 +37,26 @@ class Map extends React.Component {
     }
 
     async cambiar() {
-        if (this.state.nlayer === 1) {
-            this.state.nlayer = 2;
-            this.state.layer = layer2;
-            layer1.removeFrom(this.map);
-        }
-        else {
-            this.state.nlayer = 1;
-            this.state.layer = layer1;
-            layer2.removeFrom(this.map);
+        this.actual = this.actual+1;
+        if(this.actual >= properties.layers.length){
+            this.actual=0;
         }
 
-        this.state.layer.addTo(this.map);
+        this.layer.removeFrom(this.map);
+        this.setLayer();
     }
 
-
-    componentDidMount() {
-        this.map = L.map("map", {
-            center: [43.3602900, -5.8447600],
-            zoom: 10,
-            zoomControl: false
+    setLayer(){
+        this.layer = L.tileLayer(
+            properties.layers[this.actual], {
+            detectRetina: true,
+            maxZoom: 20,
+            maxNativeZoom: 17
         });
-        this.state.layer.addTo(this.map);
+        this.layer.addTo(this.map);
     }
 
     render() {
-
         return (
             <div className="Map">
                 <Button
@@ -81,7 +66,7 @@ class Map extends React.Component {
                     onClick={() => this.cambiar()}
                 />
                 <Wrapper id="map" />
-                <MapList handleFiles = {this.handleFiles.bind(this)} />
+                <MapList handleFiles={this.handleFiles.bind(this)} />
             </div>
 
         );

6aac1ef14541f3ee0510cfa683c8f2b1b615d2c7
samuelmorenov <UO266321@uniovi.es>
2020-04-09 19:20:35 +0200

    Añadido funcion useLDflexList para amigos

--- src/components/share/ShowFriends.js
+++ src/components/share/ShowFriends.js
@@ -1,21 +1,40 @@
 import React from "react";
-import { Value } from "@solid/react";
+import { useLDflexList } from "@solid/react";
 
+function GetFriends(src) {
+  const items = useLDflexList(src)
+    .filter(() => true)
+    .slice(0, Infinity)
+    .map(
+      (item, index) =>
+        <li name="listFriend" key={index}>
+          {`${item}`}
+          <input type="checkbox" name="amigo">
+          </input>
+        </li>
+    );
+
+  return items;
+}
 
 
 export default function ShowFriends() {
   return (
-    <div>
+    <div className="overflow">
       <form action="/action_page.php">
-        <input type="checkbox" id="http:amigo1" value="http:amigo1" />
-        <label for="http:amigo1"> amigo 1</label>
-
-        <input type="checkbox" id="http:amigo2" value="http:amigo2" />
-        <label for="http:amigo2"> amigo 2</label>
-
-        <input type="checkbox" id="http:amigo3" value="http:amigo3" />
-        <label for="http:amigo3"> amigo 3</label>
-        <input type="submit" value="Submit" />
+        <div>
+          <input type="checkbox" id="http:amigo1" value="http:amigo1" />
+          <label for="http:amigo1"> amigo 1</label>
+        </div>
+        <div>
+          <input type="checkbox" id="http:amigo2" value="http:amigo2" />
+          <label for="http:amigo2"> amigo 2</label>
+        </div>
+        <div>
+          <input type="checkbox" id="http:amigo3" value="http:amigo3" />
+          <label for="http:amigo3"> amigo 3</label>
+        </div>
       </form>
-    </div>);
+    </div>
+  );
 };

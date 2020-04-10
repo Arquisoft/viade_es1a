b45cd5bb8ac4e4b84e7ea452796e1f7ba5392d19
Alvatros96 <UO245852@uniovi.es>
2020-04-10 20:33:07 +0200

    Cambiado color base de la aplicacion

--- src/components/login/User.js
+++ src/components/login/User.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "../../static/css/login.css";
 import { useWebId, Value } from "@solid/react";
 
 export const User = () => {
   return (
     <section>
-      <div className="col-sm">
-        <span>Estas logueado como: <a href={useWebId()}><Value src="user.name"/></a></span>
+      <div className = "nav">
+        <span className>Bienvenido: <a href={useWebId()}><Value src="user.name"/></a></span>
       </div>
     </section>
   );

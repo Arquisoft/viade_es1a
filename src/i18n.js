6fbc3975a342d36a0042c58b19a4d1f510d8ac01
kevin23699 <kevin23699@gmail.com>
2020-04-16 23:01:49 +0200

    ruta backend

---
+++ src/i18n.js
@@ -0,0 +1,30 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-xhr-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+const backend = new Backend(null, { loadPath: "locales/{{lng}}/{{ns}}.json" });
+
+i18n
+  // load translation using xhr -> see /public/locales
+  // learn more: https://github.com/i18next/i18next-xhr-backend
+  .use(backend)
+  // detect user language
+  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(LanguageDetector)
+  // pass the i18n instance to the react-i18next components.
+  // Alternative use the I18nextProvider: https://react.i18next.com/components/i18nextprovider
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: false,
+    whitelist: ['en', 'es'],
+    // special options for react-i18next
+    // learn more: https://react.i18next.com/components/i18next-instance
+    react: {
+      wait: true,
+      useSuspense: false
+    }
+  });
+
+export default i18n;
--- src/i18next.js
+++ src/i18next.js
@@ -8,10 +8,12 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 // for passing in lng and translations on init
 
 const Languages= ['en','es'];
+const backend = new Backend(null, { loadPath: "locales/{{lng}}/{{ns}}.json" });
+
 i18n
   // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
   // learn more: https://github.com/i18next/i18next-http-backend
-  .use(Backend)
+  .use(backend)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)

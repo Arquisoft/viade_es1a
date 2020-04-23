45a33eb47474df05dd2cf4b579d62bd5bac711f6
samuelmorenov <UO266321@uniovi.es>
2020-04-23 11:39:04 +0200

    Añadido esqueleto de grupos

---
+++ src/components/groups/Groups.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const Hook = () => {
+
+    class Groups extends React.Component {
+        render() {
+            return (
+                <div>
+                   <p>Grupos</p>
+                </div>
+            );
+        }
+    }
+    return (<Groups />);
+};
+
+export default Hook;

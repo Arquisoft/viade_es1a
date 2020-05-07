9d627d961669c7a54511cd7a502d52a0d38797d6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-07 04:51:20 +0000

    Deploy Arquisoft/viade_es1a to github.com/Arquisoft/viade_es1a.git:gh-pages

---
+++ precache-manifest.ed57791fe3a338ae99cce04ae4388128.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1047070e26ebbf65d5a050af7acde0e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bc538d19f09a313ca56b",
+    "url": "./static/css/2.91666ac7.chunk.css"
+  },
+  {
+    "revision": "89b9e59d84a608806d94",
+    "url": "./static/css/main.712df903.chunk.css"
+  },
+  {
+    "revision": "bc538d19f09a313ca56b",
+    "url": "./static/js/2.70ccb4d6.chunk.js"
+  },
+  {
+    "revision": "0c1130ccc75d07fecb4d82de6ca7f4cb",
+    "url": "./static/js/2.70ccb4d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89b9e59d84a608806d94",
+    "url": "./static/js/main.56b508f1.chunk.js"
+  },
+  {
+    "revision": "b73901f88ec1ab541fc0",
+    "url": "./static/js/runtime-main.a31194e1.js"
+  },
+  {
+    "revision": "5e67345f592935c10d2bb8cd875928b3",
+    "url": "./static/media/ViaDe.5e67345f.svg"
+  },
+  {
+    "revision": "7dec6a4075db2deca1f9cff03658f039",
+    "url": "./static/media/campanita.7dec6a40.png"
+  }
+]);
